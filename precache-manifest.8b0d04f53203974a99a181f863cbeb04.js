5a588e4aad2b95574ed697da9e9707329e229be6
Adriam Familia <adrianfamily09@gmail.com>
2020-08-18 20:07:51 -0400

    Updates

---
+++ precache-manifest.8b0d04f53203974a99a181f863cbeb04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351d2bb5300dc85e60fb864a18f2bfce",
+    "url": "/newsapp-cms/index.html"
+  },
+  {
+    "revision": "06583857dce5aca5b200",
+    "url": "/newsapp-cms/static/css/2.5e3fcda8.chunk.css"
+  },
+  {
+    "revision": "015a2814d6f0e72e7461",
+    "url": "/newsapp-cms/static/css/main.4e673c06.chunk.css"
+  },
+  {
+    "revision": "06583857dce5aca5b200",
+    "url": "/newsapp-cms/static/js/2.959ad650.chunk.js"
+  },
+  {
+    "revision": "ae69a686e1f284d78b6ec2bbf4577426",
+    "url": "/newsapp-cms/static/js/2.959ad650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015a2814d6f0e72e7461",
+    "url": "/newsapp-cms/static/js/main.39507bfb.chunk.js"
+  },
+  {
+    "revision": "681ad180d6aac7088cd4",
+    "url": "/newsapp-cms/static/js/runtime-main.93bc134a.js"
+  }
+]);
